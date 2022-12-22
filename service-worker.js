44da344ecd94ec4be05faa20682613911ce0e12f
Feras Jobeir <fjobeir@gmail.com>
2022-12-22 14:57:45 +0300

    offline.html

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('mycache').then((cache) => {
             cache.addAll([
+                '/',
                 'index.html',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css',
                 'assets/css/common.css',
@@ -21,6 +22,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
     console.log('Activating Service Worker')
+    self.clients.claim()
 })
 
 self.addEventListener('fetch', function(event) {
